6d71445802b226322227cce1b3cd2e93af4a060f
Daniel Almeida <115008409+R4di0Uni@users.noreply.github.com>
2024-05-23 23:53:11 +0100

    e

--- main/server/database.js
+++ main/server/database.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg")
 
 const pool = new Pool({
     user:"postgres",
-    password:"ErulaZ69",
+    password:"",
     host:"localhost",
     port:5432,
     database:"mygamerhouse",
