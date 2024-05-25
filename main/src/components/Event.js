09a0b28c594a80ac252acbbd04a7b14585a317f1
RickyBoy <rmsmp2003@gmail.com>
2024-05-25 19:15:35 +0100

    aaa

--- main/src/components/Event.js
+++ main/src/components/Event.js
@@ -1,32 +1,83 @@
 import React from "react";
-import "../App.css";
+import "../styles/event_specific.css";
 
 const Event = () => {
   return (
-    <div className="event_landing_page">
-      <div className="info">
-        <h2>
-          Event
-          <span> Name</span>
-        </h2>
-        <p className="event_description">
-          Magna sint amet duis consectetur enim aute. Excepteur magna minim
-          tempor laborum laboris ipsum Lorem pariatur et pariatur. Anim nostrud
-          laborum ipsum ipsum ipsum adipisicing. Laboris irure quis incididunt
-          laborum sint excepteur nostrud est officia veniam laboris cupidatat
-          voluptate aliqua. Et cillum amet ex ipsum sint aliquip excepteur velit
-          mollit sint.
-        </p>
-        <p className="event_date">Date:</p>
-        <p className="event_location">Location:</p>
-        <p className="event_creator">Creator:</p>
+    <div class="page-event">
+      <div class="cover">
+        <div class="heading">events</div>
       </div>
-      <div className="image-container">
-        <img
-          src="https://via.placeholder.com/1000x500"
-          alt="Event Image"
-          className="event-image"
-        />
+      <div class="container">
+        <div class="upcoming-sec">
+          <div class="heading">Upcoming Events</div>
+        </div>
+        <div class="upcoming-event-list">
+          <div class="event-block">
+            <div class="row">
+              <div class="col-lg-2 sec-1">
+                <table>
+                  <tr>
+                    <td>
+                      <div class="month">jan</div>
+                      <div class="month-date-devider"></div>
+                      <div class="date">27</div>
+                    </td>
+                    <td class="title">Event Title</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="col-lg-5 sec-2">
+                <img src="https://images.unsplash.com/photo-1475721027785-f74eccf877e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGV2ZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+              </div>
+              <div class="col-lg-5 sec-3">
+                <div class="title">Event Title</div>
+                <div class="venue">
+                  <table>
+                    <tr>
+                      <td>
+                        <i class="fa fa-map-marker"></i>
+                      </td>
+                      <td>
+                        <div>Location</div>
+                        <div class="dim-color">
+                          <a href="https://www.google.co.in" target="blank">
+                            Get Directions
+                          </a>
+                        </div>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <div class="time">
+                  <table>
+                    <tr>
+                      <td>
+                        <i class="fa fa-clock-o"></i>
+                      </td>
+                      <td>
+                        <div>Date and Time</div>
+                        <div
+                          data-livestamp="1517054400"
+                          class="dim-color"
+                        ></div>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+                <div class="sort-story">Small description</div>
+                <div class="group-of-btn">
+                  <a
+                    href="https://www.google.com"
+                    target="blank"
+                    class="btn book-ticket"
+                  >
+                    Book Your Entry Pass
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
