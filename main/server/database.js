127057b1e6aef848af0973f8336fb4d2c6a652cf
Daniel Almeida <sentynelpt@gmail.com>
2024-06-06 16:32:47 +0100

    Update database.js

--- main/server/database.js
+++ main/server/database.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    hostname: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
 });
